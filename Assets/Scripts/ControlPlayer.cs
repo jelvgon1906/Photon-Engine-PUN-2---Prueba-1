1f0c8668ce84daa31767c65838d3ba9971cf1c10
jelvgon1906 <116269048+jelvgon1906@users.noreply.github.com>
2022-11-22 11:01:11 +0100

    Mapa multiplayer
    Mapa
    Personajes
    Animaciones
    GameManager

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -4,34 +4,59 @@ using UnityEngine;
 public class ControlPlayer : MonoBehaviourPunCallbacks
 {
     public float speed = 10f;
+    public float rotationSpeed;
+    Rigidbody rb;
+    Animator animator;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         //photon para que no mueva a todos los players
         if (photonView.IsMine)
-        {
-            //colocar camara en personaje
-            /*Camera.main.transform.SetParent(this.transform);
-            Vector3 posCamara = new Vector3();
-            posCamara.x = transform.position.x;
-            posCamara.y = transform.position.y + 8.94f;
-            posCamara.z = transform.position.z -3;
-            Camera.main.transform.position = posCamara;*/
+        { Move(); }
+        //colocar camara en personaje
+        /*Camera.main.transform.SetParent(this.transform);
+        Vector3 posCamara = new Vector3();
+        posCamara.x = transform.position.x;
+        posCamara.y = transform.position.y + 8.94f;
+        posCamara.z = transform.position.z -3;
+        Camera.main.transform.position = posCamara;*/
 
-            //mov
-            float movX = Input.GetAxis("Horizontal");
-            float movZ = Input.GetAxis("Vertical");
 
-            Vector3 pos = new Vector3();
-            pos.x = transform.position.x + (movX * speed * Time.deltaTime);
-            pos.y = transform.position.y;
-            pos.z = transform.position.z + (movZ * speed * Time.deltaTime);
 
-            transform.position = pos;
-        }
+       
+    }
+
+    private void Move()
+    {
+        //mov
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+
+        Vector3 velocidad = 
+              transform.right * movX * speed  // (1,0,0) * 1/-1 * speed
+            + transform.forward * movZ * speed  // (0,0,1) * 1/-1 * speed
+            + transform.up * rb.velocity.y;// (0,1,0) * rb.velocity.y
+
+        rb.velocity = velocidad;
+
+        animator.SetFloat("Velocity", velocidad.magnitude);
+
+        transform.Rotate(transform.up * Input.GetAxis("Mouse X") * rotationSpeed);
+
+        velocidad = new Vector3(velocidad.x, 0, velocidad.z);
+        animator.SetFloat("velocity", velocidad.magnitude);
+
+        /*new Vector3();
+        pos.x = transform.position.x + (movX * speed * Time.deltaTime);
+        pos.y = transform.position.y;
+        pos.z = transform.position.z + (movZ * speed * Time.deltaTime);
+
+        transform.position = pos;*/
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,22 @@
 using Photon.Pun;
 using UnityEngine;
+using Photon.Realtime;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
 
-    public static GameManager instance;
+    GameObject miPersonaje;
 
 
     private void Start()
     {
-        PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-10, 10), 10, Random.Range(-10, 10)), Quaternion.identity);
-        instance = this;
-        if (instance != null)
+        
+        PhotonNetwork.Instantiate("Player", new Vector3(1.42f, 18.38f, -3.02681f)/*(Random.Range(-10, 10), 10, Random.Range(-10, 10))*/, Quaternion.identity);
+        Camera.main.transform.SetParent(miPersonaje.transform);
+
+        /*if (instance != null)
             DontDestroyOnLoad(gameObject);
-        else Destroy(gameObject);
+        else Destroy(gameObject);*/
     }
 
 
